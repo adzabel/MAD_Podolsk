210f827df658326859bf3ea1085bd63677df95d3
MEI ZAHRA <dima@030382.ru>
2025-11-23 12:27:16 +0000

    vue 62

--- frontend/src/vue/SummaryCards.vue
+++ frontend/src/vue/SummaryCards.vue
@@ -1,5 +1,22 @@
 
 
+
+<template>
+  <div class="summary-cards">
+    <PlanCard :value="plan" :isInteractive="isInteractive" />
+    <FactCard :value="fact" :isInteractive="isInteractive" :progress="factProgress" :progressLabel="factProgressLabel" />
+    <DeviationCard :value="deviation" :isInteractive="isInteractive" />
+    <DailyAverageCard :averageValue="dailyAverage" :daysWithData="daysWithData" :isCurrentMonth="isCurrentMonth" />
+  </div>
+</template>
+
+<script setup>
+import PlanCard from './PlanCard.vue';
+import FactCard from './FactCard.vue';
+import DeviationCard from './DeviationCard.vue';
+import DailyAverageCard from './DailyAverageCard.vue';
+
+const props = defineProps({
   plan: { type: Number, default: null },
   fact: { type: Number, default: null },
   deviation: { type: Number, default: null },
@@ -11,25 +28,3 @@
   factProgressLabel: { type: String, default: "–" }
 });
 </script>
-
-
-<style>
-.summary-cards {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 24px;
-  width: 100%;
-  margin-bottom: 32px;
-}
-
-.summary-cards > * {
-  width: 100%;
-  min-width: 0;
-}
-
-@media (min-width: 900px) {
-  .summary-cards {
-    grid-template-columns: repeat(4, 1fr);
-  }
-}
-</style>
